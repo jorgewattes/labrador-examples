b9edcc98bbe9067b89e715fa44a6eb794d25d200
Your Name <you@example.com>
2025-01-14 15:36:37 -0300

    ;

--- periphery/07-pwm.py
+++ periphery/07-pwm.py
@@ -1,10 +1,12 @@
 """
 Para ativar o PWM no channel 0 do pwmchip0:
     echo 0 > /sys/class/pwm/pwmchip0/export
-    echo 20000000 > /sys/class/pwm/pwmchip0/pwm0/period  # Período de 20ms (50Hz)
-    echo 1000000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle  # Duty Cycle de 1ms
+    echo 2000000000 > /sys/class/pwm/pwmchip0/pwm0/period  # Período de 20ms (50Hz)
+    echo 1000000000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle  # Duty Cycle de 1ms
     echo 1 > /sys/class/pwm/pwmchip0/pwm0/enable
 
+$ sudo chown caninos /dev/gpiochip*
+$ sudo chmod g+rw /dev/gpiochip*
 """
 
 import time
@@ -16,8 +18,8 @@ from periphery import PWM
 
 # Open PWM chip 1, channel 8 - GPIO-B8 (header-12)
 pwm = PWM(0,0) 
-pwm.frequency = 1e3 # Set frequency to 1 kHz
-pwm.duty_cycle = 0.75 # Set duty cycle to 75%
+pwm.frequency = 1 # Set frequency to 1 kHz
+pwm.duty_cycle = 0.5 # Set duty cycle to 75%
 pwm.enable()
 
 inc=True
